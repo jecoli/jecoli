03720dc1bfbadfcc06b3b9f517221ba5e97ca19b
pvilaca <pvilaca@192.168.1.69>
2018-06-19 00:39:48 +0100

    [update] travis ignore tests

--- build/deploy.sh
+++ build/deploy.sh
@@ -37,4 +37,4 @@ sed -i -e s/DEPLOYMENT_REPO_ID/$DEPLOYMENT_REPO_ID/g ${H2_HOME}/settings.xml
 #echo GPG_PRIVATE_KEY: $GPG_PRIVATE_KEY
 #gpg -v --batch --import <(echo "$GPG_PRIVATE_KEY")
 
-mvn -Dmeworkbench.version=${MEWORKBENCH_VERSION} -s ${H2_HOME}/settings.xml -Dmaven.repo.local=$M2REPOSITORY deploy
+mvn -Dmeworkbench.version=${MEWORKBENCH_VERSION} -s ${H2_HOME}/settings.xml -Dmaven.repo.local=$M2REPOSITORY -DskipTests=true deploy
