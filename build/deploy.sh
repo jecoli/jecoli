526992c192f2616c0a1e4d64a2e6ea3220a5f32e
pvilaca <pvilaca@192.168.1.69>
2018-06-20 23:20:24 +0100

    [update] version

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,22 +1,6 @@
-MEWORKBENCH_VERSION=1.0.3
-
 
 #This variable must be iqual to id in distributionManagement.repository/snapshotRepository on pom.xml
 DEPLOYMENT_REPO_ID="oss.sonatype.org"
-
-
-echo DEPLOY_FLAG: $1
-if [ $1 = "master" ]; then
-	MEWORKBENCH_VERSION=$MEWORKBENCH_VERSION
-else
-	MEWORKBENCH_VERSION=${MEWORKBENCH_VERSION}-SNAPSHOT
-fi
-echo VERSION: $MEWORKBENCH_VERSION
-
-#echo OSS_SONATYPE_USER $OSS_SONATYPE_USER
-#echo OSS_SONATYPE_PASS $OSS_SONATYPE_PASS
-
-
 
 H2_HOME=$HOME/.deploym2
 
@@ -37,4 +21,4 @@ sed -i -e s/DEPLOYMENT_REPO_ID/$DEPLOYMENT_REPO_ID/g ${H2_HOME}/settings.xml
 #echo GPG_PRIVATE_KEY: $GPG_PRIVATE_KEY
 #gpg -v --batch --import <(echo "$GPG_PRIVATE_KEY")
 
-mvn -Dmeworkbench.version=${MEWORKBENCH_VERSION} -s ${H2_HOME}/settings.xml -Dmaven.repo.local=$M2REPOSITORY -DskipTests=true deploy
+mvn -s ${H2_HOME}/settings.xml -Dmaven.repo.local=$M2REPOSITORY -DskipTests=true deploy
